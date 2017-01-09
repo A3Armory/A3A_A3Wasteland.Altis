c20c91209f03a4793d02705a48aa7f121bf7b66c
Gigatek <a3armory@gmail.com>
2017-01-08 17:41:22 -0800

    Fix for vehicle store boat button, oops
    Thanks @ccgmatt76 for reporting this

--- client/systems/vehicleStore/dialog/vehiclestore_settings.hpp
+++ client/systems/vehicleStore/dialog/vehiclestore_settings.hpp
@@ -222,7 +222,7 @@ class vehshopd
 			sizeEx = VehStoreButton_textSize;
 
 			x = VehStoreMainBG_X + (0.0167 * SZ_SCALE);
-			y = VehStoreMainBG_Y + (0.325 * SZ_SCALE);
+			y = VehStoreMainBG_Y + (0.375 * SZ_SCALE);
 			w = 0.1173 * SZ_SCALE;
 			h = 0.040 * SZ_SCALE;
 		};
